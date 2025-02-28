903d43a3ab12367873732b33fd484b0aa126463d
gmanka <gmankab@gmail.com>
2025-02-28 11:38:09 +0400

    [u] conifg.py

--- stream_recorder/core/config.py
+++ stream_recorder/core/config.py
@@ -1,15 +1,29 @@
 from pathlib import Path
+import json
 
 
-channels_list: list['str'] = [
+default_channels_list: list['str'] = [
     'youtube.com/@jolygolf8269/live',
     'youtube.com/@IzzyLaif/live',
     'twitch.tv/jolygames',
     'twitch.tv/jolygolf',
     'twitch.tv/izzylaif',
-    'twitch.tv/kalashn1koff47',
 ]
 timeout: int = 10
 repo: Path = Path(__file__).parent.parent.parent.resolve()
-data: Path = repo / 'data'
+app_data: Path = Path('/app/data')
+data: Path
+if app_data.exists():
+    data: Path = app_data
+else:
+    data: Path = repo / 'data'
+data.mkdir(
+    parents=True,
+    exist_ok=True,
+)
+config_json: Path = data / 'config.json'
+if not config_json.exists():
+    config_json.write_text(json.dumps(default_channels_list))
+channels_list: list['str'] = json.loads(config_json.read_text())
+assert isinstance(channels_list, list)
 
